f3f1ef501b0b6410fc2c07d0da8db486efab99bd
c1ph3rC4t <c1ph3rC4t@gmail.com>
2024-07-30 19:38:22 +0200

    Update cpy.js

--- cmds/cpy.js
+++ cmds/cpy.js
@@ -1,5 +1,13 @@
-try {
-  navigator.clipboard.writeText(cmdHistory[cmd.args.l])
-} catch(err) {
-  navigator.clipboard.writeText(cmdHistory[cmdHistory.length - 1])
-}
+if (cmd.flags.includes('p')) {
+  getInput('Input: ').then((input) => {
+    navigator.clipboard.writeText(cmdHistory[cmdHistory.length - 1 - input]);
+    console.log(input);
+  });
+
+} else if ('i' in cmd.args) {
+  navigator.clipboard.writeText(cmdHistory[cmdHistory.length - 1 - cmd.args.i]);
+} else if ('item' in cmd.args) {
+  navigator.clipboard.writeText(cmdHistory[cmdHistory.length - 1 - cmd.args.item]);
+} else {
+  navigator.clipboard.writeText(cmdHistory[cmdHistory.length - 2]);
+};
