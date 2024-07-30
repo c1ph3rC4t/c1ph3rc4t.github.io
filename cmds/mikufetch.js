cffe2a9ffe0d05221ce46b01d0b5b1ba1e412162
c1ph3rC4t <c1ph3rC4t@gmail.com>
2024-07-30 17:16:34 +0200

    Create mikufetch.js

---
+++ cmds/mikufetch.js
@@ -0,0 +1,19 @@
+print(`⣿⣿⣿⡿⠋⣱⠊⠁⣴⣲⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⡭⠋⡴⠽⠁⣰⣭⡻⣿ ${username}@${device}
+⣿⣿⣿⣥⡈⠁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡃⠞⠇⢀⣴⢿⣿⣿⣾ ${`${username}@${device}`.replace(/./g, '-')}
+⣿⣿⣷⣿⢣⣾⣿⣿⡟⣿⣿⣿⣿⡿⢻⣿⣿⣿⣿⣿⣷⡀⢀⢀⣽⡻⣿⣿⣿ OS: Miku Linux
+⣿⣿⣿⡵⢟⣿⣿⡟⢠⢻⢿⣿⡿⣧⢠⢻⣿⣧⢻⣹⣿⣧⢀⢀⠘⡇⣹⣿⣿ Kernel: 6.8.11-amd64
+⣿⣿⣿⢏⣿⣿⡿⢀⣻⡞⠘⣿⣷⢻⢘⣇⠛⣿⣏⢇⣿⣿⣇⣤⢀⡄⢙⣿⣿ Uptime: ${formatTime(getUptime())}
+⣿⣿⡿⣾⣿⣿⢡⠧⠿⣷⡐⡹⣿⡜⢸⡿⢷⡙⢿⣞⢻⣿⣿⠉⠘⠁⡎⢿⣿ Packages: 12683 (dpkg)
+⣿⣿⣧⣿⣿⡇⢈⣴⡆⢀⢷⡘⢽⣇⠆⣥⣶⢀⠙⠻⠸⣿⣿⠐⠈⢀⣿⢸⣿ Shell: zsh 5.9
+⣿⡟⡟⣿⣿⠃⢺⣿⢠⡠⣼⣷⣵⣽⣸⣿⡇⣀⡀⣄⢦⣿⡟⢀⢀⢸⣿⣸⣿ Resolution: 3840x2160
+⣿⣾⡇⢿⢿⢀⡌⣿⣷⣶⣿⣿⣿⣿⣿⣿⣷⣭⣵⠇⣾⣿⡇⢀⢀⣸⣿⣿⣿ DE: Xfce 4.18
+⣿⣿⣏⠘⢏⣚⠣⡹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣫⠘⣼⣿⠃⢰⣿⣿⣿⣿⣿ WM: Xfwm4
+⣿⣿⣿⣿⢨⠻⣿⣷⣯⣻⢿⣿⣿⣿⣿⣿⣿⢟⢋⢸⣿⠻⠘⣸⠿⡿⣿⣿⣿ WM Theme: Miku-dark
+⣹⣿⣿⣿⢀⢀⢈⡻⣿⣿⣷⡙⠻⠟⣫⡽⣞⣵⢏⣿⠇⢀⣰⣵⣿⣿⣿⣿⣿ Theme: Miku-dark
+⣿⣿⣿⣿⢸⣰⣼⢮⣾⣿⣿⠏⣺⡁⣶⡿⢟⣅⠞⣡⡴⣰⣿⣿⣿⣿⣿⣿⣿ Terminal: Mikuminal
+⣿⣿⣿⣿⢰⡿⣫⣿⣿⣿⡏⣼⡟⡙⠌⢺⣿⣿⣟⣿⢣⢟⠻⠿⣿⣿⣿⣿⣿ Terminal Font: FiraCode 10
+⣿⣿⣿⡏⠟⢀⣿⣿⣿⣟⣤⣏⠆⣿⠲⡊⣿⣾⣿⢣⣏⢋⡞⣸⣿⣿⣿⣿⣿ CPU: C1ph3r® Corp. standard issued CPUs
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ GPU: C1ph3r® Corp. standard issued GPUs
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ NPU: C1ph3r® Corp. standard issued NPUs
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ Storage: 956PiB / 1EiB
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ Memory: 162PiB / 256PiB`)
