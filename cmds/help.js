ae4a51d01496b0311c067f310e46126afc12bf99
c1ph3rC4t <c1ph3rC4t@gmail.com>
2024-07-30 19:40:29 +0200

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,1 +1,4 @@
-print(`Here is a list of all commands at the moment: ${cmds}`)
+print('Here is a list of all commands available at the moment:')
+for (let i = 0; i < cmds.length; i++) {
+  print(cmds[i])
+}
