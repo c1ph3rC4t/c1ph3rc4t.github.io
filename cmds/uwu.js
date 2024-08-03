4538e110f76b02fdc4d0d84326232c609129b32d
c1ph3rC4t <c1ph3rC4t@gmail.com>
2024-08-03 14:27:44 +0200

    Update uwu.js

--- cmds/uwu.js
+++ cmds/uwu.js
@@ -1,3 +1,3 @@
-if (hasFlag('c') || hasFlag('control') || hasFlag('ctrl)) {
-  redir(`https://c1ph3rc4t.github.io/UwU-IP-grabber-control-pannel/`);
+if (hasFlag('c') || hasFlag('control') || hasFlag('ctrl')) {
+  redir('https://c1ph3rc4t.github.io/UwU-IP-grabber-control-pannel/');
 };
